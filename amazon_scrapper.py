2bf2655a16bacc1a30d982fbbb0f8290da0faba6
Lino Rallo <linorallo@gmail.com>
2020-02-01 17:38:59 -0300

    Filtrado de items mejorado y encuentra opcion mas barata

--- amazon_scrapper.py
+++ amazon_scrapper.py
@@ -15,7 +15,7 @@ results=[]
 while currentPage < searchPageDepth : 
     if currentPage != 0 :
         if currentPage <= searchPageDepth : 
-            urlSite = 'amazon.com' + str(page_soup.find('li', {'class':'a-last'}).a['href']) + '/'
+            urlSite = 'https://amazon.com' + str(page_soup.find('li', {'class':'a-last'}).a['href']) + '/'
             webSite = urlReq(urlSite)
             html = webSite.read()
             webSite.close()
@@ -23,25 +23,29 @@ while currentPage < searchPageDepth :
     itemsWhole = page_soup.findAll('span',{'cel_widget_id':'SEARCH_RESULTS-SEARCH_RESULTS'})
     for item in itemsWhole:
         text = str(item.find('span',{'class':'a-size-medium a-color-base a-text-normal'}))
-        blockedItem = False
         for blockedWord in blockWords:
-            if blockedWord in text:
-                blockedItem = True
-        if blockedItem == False:
-            name=text.strip('<span class="a-size-medium a-color-base a-text-normal" dir="auto">').strip('</')
-            price = str(item.find('span',{'class':'a-price-whole'})).strip('<span class="a-price-whole">').strip('<span class="a-price-decimal">.</span></')
-            itemNumber = str(len(results))
-            #link = 'amazon.com' + str(item.find('a', {'class':'a-size-base a-link-normal s-no-hover a-text-normal'})).strip('<a class="a-size-base a-link-normal s-no-hover a-text-normal" href="').strip(">")
-            link = 'amazon.com' + item.find('div',{'class':'a-row a-size-base a-color-base'}).a['href'] + '/'
-            results.append((itemNumber, price, name, link,))
-            print("item #"+ itemNumber +": "+ name +" $"+ str(price))
+            if blockedWord not in text:
+                #if searchString in text :
+                    name=text.strip('<span class="a-size-medium a-color-base a-text-normal" dir="auto">').strip('</')
+                    price = str(item.find('span',{'class':'a-price-whole'})).strip('<span class="a-price-whole">').strip('<span class="a-price-decimal">.</span></')
+                    itemNumber = str(len(results)+1)
+                    #link = 'amazon.com' + str(item.find('a', {'class':'a-size-base a-link-normal s-no-hover a-text-normal'})).strip('<a class="a-size-base a-link-normal s-no-hover a-text-normal" href="').strip(">")
+                    link = 'amazon.com' + item.find('div',{'class':'a-row a-size-base a-color-base'}).a['href'] + '/'
+                    results.append((itemNumber, int(price), name, link,))
+                    print("item #"+ itemNumber +": "+ name +" $"+ str(price))
     currentPage=currentPage+1
-    with open(searchString+".cvs", mode='w') as csv_file:
-        fieldnames=['item #','price','title', 'link']
-        writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
-        writer.writeheader()
-        for result in results:
-            #writer.writerow(result[int(itemNumber)])
-            writer.writerow({'item #':result[0],'price':result[1],'title':result[2], 'link' : result[3]})
-                
+minResultPrice = 100000000000000000
+minResultItemNum = ''
+with open(searchString+".cvs", mode='w') as csv_file:
+    fieldnames=['item #','price','title', 'link']
+    writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
+    writer.writeheader()
+    for result in results:
+        #writer.writerow(result[int(itemNumber)])
+        if result[1] < minResultPrice :
+            minResultPrice = result[1]
+            minResultItemNum = result[0]
+        writer.writerow({'item #':result[0],'price':result[1],'title':result[2], 'link' : result[3]})
+print('Cheapest choice: item #' + minResultItemNum + ' @ $' + str(minResultPrice))
+
     
