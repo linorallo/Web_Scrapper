444ef737c6df7166722b8982d6c4797e4cf554b5
Lino Rallo <linorallo@gmail.com>
2020-02-04 20:53:51 -0300

    newegg added

--- newegg_scrapper.py
+++ newegg_scrapper.py
@@ -1,45 +1,52 @@
 import bs4
 from urllib.request import urlopen as urlReq
 from bs4 import BeautifulSoup as soup 
-import csv
-searchString = "asus"
-blockWords = ["None", 'screen', 'Protector', 'case', 'film']
-searchPageDepth = 4
-results=[]
-currentPage = 1
-while currentPage < searchPageDepth : 
-    if currentPage != 0 :
-        if currentPage <= (searchPageDepth + 1) : 
-            urlSite = "https://www.newegg.com/p/pl?d=" + searchString + "&Page=" + str(currentPage)
-            webSite = urlReq(urlSite)
-            html = webSite.read()
-            webSite.close()
-            page_soup = soup(html, 'html.parser')
-    itemsWhole = page_soup.findAll('siv',{'class':'item-container'})
-    for item in itemsWhole:
-        text = str(item.find('div',{'class':'item-info'}))
-        #if searchString in text :
-        for blockedWord in blockWords:
-            if blockedWord not in text:                
-                name=text.a
-                price = text.find('li',{'class':'price-current'}).strong
+def searchInNewegg(searchString, blockedWord, strictFilter, searchPageDepth):
+    if len(blockedWord) > 0:
+        blockedWordFilter = True
+    else :
+        blockedWordFilter = False
+    results=[]
+    currentPage = 1
+    while currentPage <= searchPageDepth : 
+        if currentPage != 0 :
+            if currentPage <= (searchPageDepth + 1) : 
+                urlSite = "https://www.newegg.com/p/pl?d=" + searchString + "&Page=" + str(currentPage)
+                webSite = urlReq(urlSite)
+                html = webSite.read()
+                webSite.close()
+                page_soup = soup(html, 'html.parser')
+        itemsWhole = page_soup.findAll('div',{'class':'item-container'})
+        for item in itemsWhole:
+            def itemAnalysis():
+                text = item.find('div',{'class':'item-info'})
+                name=str(text.find('a',{'class':'item-title'}).text)
+                price = str(text.find('li',{'class':'price-current'}).text)
+                print('price: '+ price)
+                discount = str(text.find('span',{'class':'price-save-percentage'}))
+                print('discount: '+discount)
                 itemNumber = str(len(results)+1)
                 link = text.a['href']
-                results.append((itemNumber, int(price), name, link,))
-                print("item #"+ itemNumber +": "+ name +" $"+ str(price))
-    currentPage=currentPage+1
-minResultPrice = 100000000000000000
-minResultItemNum = ''
-with open(searchString+".cvs", mode='w') as csv_file:
-    fieldnames=['item #','price','title', 'link']
-    writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
-    writer.writeheader()
-    for result in results:
-        #writer.writerow(result[int(itemNumber)])
-        if result[1] < minResultPrice :
-            minResultPrice = result[1]
-            minResultItemNum = result[0]
-        writer.writerow({'item #':result[0],'price':result[1],'title':result[2], 'link' : result[3]})
-print('Cheapest choice: item #' + minResultItemNum + ' @ $' + str(minResultPrice))
+                if len(discount) <2 :
+                    discount = '0%'
+                results.append((itemNumber, price, name, link, discount))
+                print("item #"+ itemNumber +": "+ name +" $"+ str(price) + " OFF " + discount)  
+            if blockedWordFilter == True :
+                for bWord in blockedWord:
+                    if bWord not in str(item):  
+                        if strictFilter == True :
+                            if searchString in str(item) :
+                                itemAnalysis()    
+                        else:
+                            itemAnalysis()        
+            else :
+                if strictFilter == True :
+                    if searchString  in str(item) :
+                        itemAnalysis()    
+                else:
+                    itemAnalysis() 
+        currentPage=currentPage+1
+    return results
+    #csvWriter.writeOut(results, searchString)
 
-    
+        
