a769e46f6184a47cb0d77cd5c4ef417d5a5ebb4b
Lino Rallo <linorallo@gmail.com>
2020-02-04 23:40:30 -0300

    newegg done

--- newegg_scrapper.py
+++ newegg_scrapper.py
@@ -1,11 +1,9 @@
 import bs4
+import sortResults
 from urllib.request import urlopen as urlReq
 from bs4 import BeautifulSoup as soup 
-def searchInNewegg(searchString, blockedWord, strictFilter, searchPageDepth):
-    if len(blockedWord) > 0:
-        blockedWordFilter = True
-    else :
-        blockedWordFilter = False
+def searchInNewegg(searchString, blockedWord, searchPageDepth, sortPreference):
+    searchString = searchString.replace(' ','+')
     results=[]
     currentPage = 1
     while currentPage <= searchPageDepth : 
@@ -16,37 +14,31 @@ def searchInNewegg(searchString, blockedWord, strictFilter, searchPageDepth):
                 html = webSite.read()
                 webSite.close()
                 page_soup = soup(html, 'html.parser')
-        itemsWhole = page_soup.findAll('div',{'class':'item-container'})
+        itemsWholeGrid = page_soup.find('div',{'class':'items-view is-grid'})
+        itemsWhole = itemsWholeGrid.findAll('div',{'class':'item-container'})
         for item in itemsWhole:
             def itemAnalysis():
+                print('--------------------------------')
                 text = item.find('div',{'class':'item-info'})
                 name=str(text.find('a',{'class':'item-title'}).text)
-                price = str(text.find('li',{'class':'price-current'}).text)
-                print('price: '+ price)
+                price = str(text.find('li',{'class':'price-current'}))[78:85].strip('</strong>').replace(',','')
                 discount = str(text.find('span',{'class':'price-save-percentage'}))
-                print('discount: '+discount)
                 itemNumber = str(len(results)+1)
                 link = text.a['href']
                 if len(discount) <2 :
                     discount = '0%'
                 results.append((itemNumber, price, name, link, discount))
                 print("item #"+ itemNumber +": "+ name +" $"+ str(price) + " OFF " + discount)  
-            if blockedWordFilter == True :
-                for bWord in blockedWord:
-                    if bWord not in str(item):  
-                        if strictFilter == True :
-                            if searchString in str(item) :
-                                itemAnalysis()    
-                        else:
-                            itemAnalysis()        
-            else :
-                if strictFilter == True :
-                    if searchString  in str(item) :
-                        itemAnalysis()    
-                else:
-                    itemAnalysis() 
+            bWordFound=0
+            for bWord in blockedWord:
+                    if bWord in str(item):
+                        bWordFound+=1
+            if bWordFound == 0 :
+                itemAnalysis()
         currentPage=currentPage+1
-    return results
-    #csvWriter.writeOut(results, searchString)
+    if sortPreference == 'Increasing' :
+        return sortResults.sortIncreasing(results)
+    if sortPreference == 'Decreasing' :
+        return sortResults.sortDecreasing(results)
 
         
