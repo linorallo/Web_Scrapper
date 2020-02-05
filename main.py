cc46d6db4edde3b590eef291b5e7c13333107e9e
Lino Rallo <linorallo@gmail.com>
2020-02-04 23:40:45 -0300

    main updated

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import amazon_v2
 import newegg_scrapper
-searchString = "moto"
-blockWords = ["None", 'screen', 'Protector', 'case', 'film']
-strictFilter = False
-searchPageDepth = 2
-neweggResults = newegg_scrapper.searchInNewegg(searchString,blockWords,strictFilter,searchPageDepth)
-#amazonResults = amazon_v2.searchInAmazon(searchString,blockWords,strictFilter,searchPageDepth)
-#print(amazonResults)
+import sortResults
+import csvWriter
+searchString = "hp Ryzen"
+blockWords = ['Celeron', 'i7']
+sortPreference = 'Increasing'
+searchPageDepth = 1
+neweggResults = newegg_scrapper.searchInNewegg(searchString,blockWords,searchPageDepth, sortPreference)
+amazonResults = amazon_v2.searchInAmazon(searchString,blockWords,searchPageDepth, sortPreference)
+results = sortResults.sortIncreasing(neweggResults+amazonResults)
+csvWriter.writeOut(results, searchString)
+print('Search Completed')
