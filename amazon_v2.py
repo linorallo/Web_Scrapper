297282c9cda1c01abd47670352dc62dabe385fe6
Lino Rallo <linorallo@gmail.com>
2020-02-15 09:32:14 -0300

    amazonScrapper modified
    Conexion a db Updated
    scrapping updated

--- amazon_v2.py
+++ amazon_v2.py
@@ -2,7 +2,10 @@ import bs4
 import sortResults
 from urllib.request import urlopen as urlReq
 from bs4 import BeautifulSoup as soup 
+import datetime as date
+amazonDBPK = 3
 def searchInAmazon(searchString, blockedWord, searchPageDepth, sortPreference, currency):
+    datetime = date.datetime.now()
     searchString = searchString.replace(' ','+')
     currentPage = 0
     urlSite = "https://www.amazon.com/s?k=" + searchString + "&ref=nb_sb_noss_2"
@@ -24,25 +27,27 @@ def searchInAmazon(searchString, blockedWord, searchPageDepth, sortPreference, c
             def itemAnalysis():
                 if 'App' and 'Prime Video' not in str(item):
                     text = str(item.find('span',{'class':'a-size-medium a-color-base a-text-normal'}))
-                    #print('--------------------------------')
                     name=text.strip('<span class="a-size-medium a-color-base a-text-normal" dir="auto">').strip('</')
                     if (item.find('free') or item.find('FREE')):
                         price = fullPrice = '0'
                     else:
-                        #print(name)
-                        price = str(item.find('span',{'class':'a-price-whole'}))[28:37].strip('<span class')
-                        fullPrice = str(item.find('span',{'class':'a-offscreen'}))[27:36].strip('</span ')
                         try:
-                            if float(fullPrice) - float(price) > 1:
-                                discount = str(100-(float(price)*100/float(fullPrice))) + '%'
-                            else:
-                                discount = 'N/A'  
-                        except ValueError:
-                            discount = 'N/A'     
-                    itemNumber = str(len(results))
-                    link = 'https://amazon.com' + item.find('div',{'class':'a-row a-size-base a-color-base'}).a['href'] + '/'
-                    results.append((itemNumber, price, name, link, discount))
-                    #print("item #"+ itemNumber +": "+ name +" $"+ str(price) + ' OFF ' + discount +' !')
+                            price = str(item.find('span',{'class':'a-price-whole'}).text).strip('.').replace(',','')
+                            #price = str(item.find('span',{'class':'a-price-whole'}))[28:37].strip('<span class')
+                            fullPrice = str(item.find('span',{'class':'a-offscreen'}))[27:36].strip('</span ')
+                            try:
+                                if float(fullPrice) - float(price) > 1:
+                                    discount = str(100-(float(price)*100/float(fullPrice))) + '%'
+                                else:
+                                    discount = '0'  
+                            except ValueError as err:
+                                discount = '0'     
+                            itemNumber = str(len(results))
+                            link = ('https://amazon.com' + item.find('a',{'class':'a-link-normal a-text-normal'})['href']).partition('ref')[0]
+                            results.append((itemNumber, price, name, link, discount, str(datetime), amazonDBPK))
+                        except AttributeError as err:
+                            print('Item Skipped due to: ' +str(err))
+                    
             bWordFound = 0
             for bWord in blockedWord:
                 if bWord in str(item):  
