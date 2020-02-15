2f674553f47d37d12ea400ed8c592be33c7c0c56
Lino Rallo <linorallo@gmail.com>
2020-02-15 09:33:14 -0300

    NeweggScrapper creado

--- newegg_scrapper.py
+++ newegg_scrapper.py
@@ -1,11 +1,14 @@
 import bs4
 import sortResults
+import datetime as date
 from urllib.request import urlopen as urlReq
 from bs4 import BeautifulSoup as soup 
+neweggDBPK = 1
 def searchInNewegg(searchString, blockedWord, searchPageDepth, sortPreference, currency):
     searchString = searchString.replace(' ','+')
     results=[]
     currentPage = 1
+    datetime = date.datetime.now()
     while currentPage <= searchPageDepth : 
         if currentPage != 0 :
             if currentPage <= (searchPageDepth + 1) : 
@@ -17,22 +20,26 @@ def searchInNewegg(searchString, blockedWord, searchPageDepth, sortPreference, c
         itemsWholeGrid = page_soup.find('div',{'class':'items-view is-grid'})
         itemsWhole = itemsWholeGrid.findAll('div',{'class':'item-container'})
         for item in itemsWhole:
+            
             def itemAnalysis():
                 #print('--------------------------------')
                 text = item.find('div',{'class':'item-info'})
                 name=str(text.find('a',{'class':'item-title'}).text)
                 price = str(text.find('li',{'class':'price-current'}))[78:85].strip('</strong>').replace(',','')
-                discount = str(text.find('span',{'class':'price-save-percentage'}))
+                discount = str(text.find('span',{'class':'price-save-percentage'})).strip('%')
                 itemNumber = str(len(results)+1)
-                link = text.a['href']
+                link = str(text.find('a',{'class':'item-title'})['href']).partition('?')[0]
                 if len(discount) <2 :
                     discount = '0%'
-                results.append((itemNumber, price, name, link, discount))
-                #print("item #"+ itemNumber +": "+ name +" $"+ str(price) + " OFF " + discount)  
-            bWordFound=0
+                if discount == 'None' :
+                    discount = 0
+                results.append((str(itemNumber), str(price), name, link, str(discount), str(datetime) ,neweggDBPK))
+                
+                #print("item #"+ itemNumber +": "+ name +" $"+ price + ' OFF: '+ discount )
+            bWordFound = 0
             for bWord in blockedWord:
-                    if bWord in str(item):
-                        bWordFound+=1
+                if bWord in str(item):  
+                    bWordFound+=1
             if bWordFound == 0 :
                 itemAnalysis()
         currentPage=currentPage+1
