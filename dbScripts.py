71a3a624337bd3ac79821bd8d7fe96b1c31f7715
Lino Rallo <linorallo@gmail.com>
2020-02-15 09:30:58 -0300

    dbScripts.py Creado

---
+++ dbScripts.py
@@ -0,0 +1,21 @@
+def deleteDuplicates() :
+    return 'DELETE FROM product    WHERE idproduct NOT IN      (  SELECT MAX(idproduct)    GROUP BY productLink  )'
+
+def retrieveAll():
+    return 'SELECT productLink, productPrice FROM  product'
+
+def updatePriceProduct(linkProduct, newPrice):
+    return 'UPDATE product SET priceProduct = ' + newPrice + ' WHERE linkProduct  = ' + linkProduct + ';'
+
+def searchProducts(searchList, blockedWords) :
+    query = 'SELECT itemNumber, productPrice, productName, productLink FROM product WHERE '
+    iterationNumber=0
+    for searchWord in searchList :
+        if iterationNumber == 0 :
+            query = query + ' productName LIKE "%' + searchWord + '%" '
+            iterationNumber += 1
+        else:
+            query = query + 'AND productName LIKE "%' + searchWord + '%" '
+    for blockedWord in blockedWords :
+        query = query + 'AND productName NOT LIKE "%' + blockedWord + '%" ' 
+    return query + ' ;'
