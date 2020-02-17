ff510e94e52bd12c092fb8839aa264308068bc4a
Lino Rallo <linorallo@gmail.com>
2020-02-17 14:52:51 -0300

    Auto stash before merge of "master" and "origin/master"

--- csvWriter.py
+++ csvWriter.py
@@ -11,8 +11,11 @@ def writeOut(results, searchString):
     print ('Entro a csvWriter')
     with open(searchString+".cvs", mode='w') as csv_file:
             fieldnames=['item #','price','title', 'discount', 'link',]
-            writer = csv.writer(csv_file, lineterminator='\n')
-            #writer.writeheader()
+            writer = csv.DictWriter(csv_file,fieldnames=fieldnames)
+            writer.writeheader()
             for result in results:
-                writer.writerow(result)    
+                try:
+                    writer.writerow({'item #':result[0],'price':result[1],'title':result[2], 'discount':result[4], 'link' : result[3]})    
+                except UnicodeEncodeError :
+                    print('UnicodeEncodeError')
                 #writer.write
