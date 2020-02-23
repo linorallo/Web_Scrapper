f70bd89db3971903fd3d0673525a2bdbdc46b738
Lino Rallo <linorallo@gmail.com>
2020-02-23 13:17:16 -0300

    Contact page created

---
+++ web/product-finder/src/components/pages/contact.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Contact extends Component{
+    render(){
+        return(
+            <div className='container-fluid'>
+                <h1>   
+                    Our Contact Information!
+                </h1>
+                <p>
+                    If you wish to get in touch with us, please send an email to support@productfinder.com
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Contact;
