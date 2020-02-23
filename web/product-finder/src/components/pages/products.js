1c101836216ff8d683c6e7c8c05e89db1612a928
Lino Rallo <linorallo@gmail.com>
2020-02-23 13:17:24 -0300

    product page created

---
+++ web/product-finder/src/components/pages/products.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Products extends Component{
+    render(){
+        return(
+            <div className='container-fluid'>
+                <h1>   
+
+                </h1>
+                <p>
+                    Product description
+                </p>
+            </div>
+        )
+    }
+}
+
+export default Products;
