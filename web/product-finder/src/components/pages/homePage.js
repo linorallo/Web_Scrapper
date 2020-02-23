f9e6fcd2015c8339dc0a5dddbe9fef2dc88f84c2
Lino Rallo <linorallo@gmail.com>
2020-02-23 13:16:49 -0300

    homePage updated with router

--- web/product-finder/src/components/pages/homePage.js
+++ web/product-finder/src/components/pages/homePage.js
@@ -4,8 +4,12 @@ import React from 'react';
 function Homepage ()  {
   return (
     <div className="container-fluid">
-       Home Page Content
-       
+      <h1>
+      ¿Qué te atrajo de la revolución soviética para llevarla a “1917. Traición y revolución”?
+      </h1>
+      <p>
+        El tema de Rusia, desde los zares hasta le revolución soviética me ha apasionado desde siempre. Es raro porque en mi familia no había ni escritores, ni intelectuales, ni historiadores. Nadie era ruso, comunista o republicano español, ningún vínculo. A partir de mis 15 dejé la prepa oficial para hacerla abierta y poder dedicarme a estudiar música. Comencé a estar más cercano a temas socialistas, tuve mi etapa marxista, no como revolucionario o activista, sino como soñador, como idealista, como filósofo y después como historiador. Con Marx y Nietzsche me pasó como con lo soviético: simplemente me gusta desde que me acuerdo. Dejando el pasado. En la actualidad no deja de apasionarme el tema de las revoluciones, así como antes fue por la parte idealista, la de un pueblo como un ser luchando contra la opresión, pasé al lado contrario: el fracaso y fraude total de las revoluciones. Fue cuando, en esa mezcla de historia y meditación en que vivo descubrí que la única revolución que puede triunfar es la interna. Y claro, no podía dejar pasar los 100 años de la revolución soviética. https://www.milenio.com/estados/zunzunegui-revela-misterios-historia-rusa-mexico
+      </p> 
     </div>
   );
 }
