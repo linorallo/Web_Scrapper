5aad8787104458f179a8da24c63da2a652ede040
Lino Rallo <linorallo@gmail.com>
2020-02-23 13:12:41 -0300

    App.js updated to support routing and styles

--- web/product-finder/src/App.js
+++ web/product-finder/src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-//import './Assets/css/default.min.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+//components
+import './Assets/css/styles.min.css';
 import Header from './components/headerComponents/header';
 import Footer from './components/footerComponent/footer';
 import Homepage from './components/pages/homePage';
+import Products from './components/pages/products';
+import Contact from './components/pages/contact';
 
+//includes
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Homepage/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/Products' component={Products} />
+        <Route exact path='/contact'component={Contact} />
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
